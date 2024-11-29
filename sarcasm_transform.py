716016d167335dfe13e3d0e6c0b7a3f49e9b1d3f
gerin <denapratama7@gmail.com>
2024-11-30 01:20:53 +0700

    update

--- sarcasm_transform.py
+++ sarcasm_transform.py
@@ -14,7 +14,11 @@ def preprocessing_fn(inputs):
     Return:
         outputs: map from feature keys to transformed features.    
     """
+    
     outputs = {}
+    
     outputs[transformed_name(FEATURE_KEY)] = tf.strings.lower(inputs[FEATURE_KEY])
+    
     outputs[transformed_name(LABEL_KEY)] = tf.cast(inputs[LABEL_KEY], tf.int64)
+    
     return outputs
